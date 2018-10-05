97dca2cd6d3ae8b915e409c89d529fef3dba73c2
00King00 <mr.1bilous@gmail.com>
2018-10-06 00:13:50 +0300

    add footer

--- dev/js/common.js
+++ dev/js/common.js
@@ -5,4 +5,20 @@ $('select').niceSelect();
 $('.pgs-nav ul').scrollbar();
 
 // end pgs-panel
+$(window).resize(function(){
+	var browserMinWidth = $(window).width();
+	if (browserMinWidth >= 800){
+		$(".footer__list").show();
+	}
+	
+});
+$('.footer__title').click(function(){
+	var browserMinWidth = $(window).width();
+	console.log(browserMinWidth)
+		if (browserMinWidth < 800){
+			$(this).next('.footer__list').slideToggle("slow");
+		}
+	
+});
+	
 });
